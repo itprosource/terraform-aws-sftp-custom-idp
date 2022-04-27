18326c7594d2b365469c0f3c198244c5da59d8d6
austin-thome <78614344+austin-thome@users.noreply.github.com>
2022-04-27 12:51:58 -0400

    Removed incorrect / from secret string HomeDirectory

--- secrets_mgr.tf
+++ secrets_mgr.tf
@@ -2,13 +2,7 @@ resource "aws_secretsmanager_secret" "secret" {
   for_each = var.idp_users
   name = "SFTP/${each.key}"
 }
-/*
-resource "aws_secretsmanager_secret_version" "secrets" {
-  for_each      = var.secrets
-  secret_id     = aws_secretsmanager_secret.secret[each.key].id
-  secret_string = jsonencode(each.value)
-}
-*/
+
 resource "aws_secretsmanager_secret_version" "secrets" {
   for_each      = var.idp_users
   secret_id     = aws_secretsmanager_secret.secret[each.key].id
