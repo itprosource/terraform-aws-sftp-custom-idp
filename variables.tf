38b72c1608af63a51f6116dfb660db2c74128dd9
Austin Thome <austin.thome1@gmail.com>
2022-08-13 11:04:51 -0500

    Filled out descriptions and removed empty defaults.

--- variables.tf
+++ variables.tf
@@ -1,89 +1,54 @@
 variable "name" {
   type = string
-  description = ""
-  default = ""
+  description = "Name used for identifying resources."
 }
 
 variable "cidr" {
   type = string
-  description = ""
-  default = ""
+  description = "CIDR range for VPC."
 }
 
 variable "subnet_cidr" {
   type = string
-  description = ""
-  default = ""
+  description = "Subnet CIDR range for SFTP server."
 }
 
 variable "az" {
   type = string
-  description = ""
-  default = ""
+  description = "Availability zone for SFTP solution."
 }
 
 variable "ingress_rules" {
   type = map(map(any))
-  description = ""
-  default = {}
+  description = "Security group ingress rules for SFTP server."
 }
 
 variable "bucket_name" {
   type = string
-  description = ""
-  default = ""
-}
-
-variable "bucket_arn" {
-  type = string
-  description = ""
-  default = ""
+  description = "Name of s3 bucket used for holding files."
 }
 
 variable "security_policy_name" {
   type = string
-  description = ""
-  default = ""
-}
-
-variable "secrets" {
-  type = map(string)
-  description = ""
-  default = {}
+  description = "Name of security policy to apply to SFTP server."
 }
 
 variable "rest_api_name" {
   type = string
-  description = ""
-  default = ""
+  description = "Identifying name for REST API used for iDP."
 }
 
 variable "function_name" {
   type = string
-  description = ""
-  default = ""
-}
-
-variable "create_s3_bucket" {
-  type = bool
-  description = ""
-  default = false
-}
-
-variable "folders" {
-  type = map(string)
-  description = ""
-  default = {}
+  description = "Name of Lambda function which controls iDP process."
 }
 
 variable "idp_users" {
   type = map(map(string))
-  description = ""
-  default = {}
+  description = "Map of user values - username, passwords, home directories."
 }
 
 variable "region" {
   type = string
-  description = ""
-  default = ""
+  description = "Region in which to host SFTP server."
 }
